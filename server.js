efd35dd5e6f1611abbc10d227a4f1fc672fa5b11
Ahmed Jafri <ajafri19@gmail.com>
2015-03-01 17:52:18 -0800

    Adding sequelize to server.js

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 'use strict';
 
 var app = require('./routes');
-
+var Sequelize = require('sequelize');
 var port = process.env.PORT || 3000;
+
 
 app.listen(port, function() {
   console.log('server running on port: %d', port);
 });
+
+var sequelize = new Sequelize('thirteen', 'thirteen', 'ZPMKZ82fhNjXDD78', {
+  host: 'localhost',
+  dialect: 'mysql',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+});
+
+var User = require("./models/Users")(sequelize);
+
 
 module.exports = app;
