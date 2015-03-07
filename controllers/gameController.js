6434ffe8ddc7920510f06dfe1c8bfb64e94d0aab
Ahmed Jafri <ajafri19@gmail.com>
2015-03-07 14:17:38 -0800

    Send a meaningful response and do some validation.

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -8,6 +8,18 @@ module.exports = {
         var gameCreatorUserId = req.body.gameCreatorUserId;
         friendUserIds.push(gameCreatorUserId);
 
+        // before we save anything to the mysql lets sanitize 
+        // the input and return appropriate errors
+        // [TODO] - do we need any other validation?
+        // [TODO] - more meaningful error messages
+        friendUserIds.forEach(function(id) {
+            if(!id){
+                res.status(400) // 400 Bad Request
+                res.send("error")
+                return;
+            }
+        });
+
         var playerCreatePromises = [];
         friendUserIds.forEach(function(id) {
             playerCreatePromises.push(db.Player.create({
@@ -18,7 +30,9 @@ module.exports = {
         db.Game.create().then(function(game){
             Q.all(playerCreatePromises)
                 .then(function(playerInstances) {
-                    game.setPlayers(playerInstances).done(function(){ res.send() });
+                    game.setPlayers(playerInstances).then(function(){ 
+                        res.send(game) 
+                    });
                 })
         });
     }
