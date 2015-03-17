66fa916de25657c5fd1e2271f8137ce24b2ed32b
Ahmed Jafri <ajafri19@gmail.com>
2015-03-16 18:31:25 -0700

    hide game endpoints behind /api/ which requires authentication

--- routes.js
+++ routes.js
@@ -2,13 +2,24 @@
 
 var bodyParser = require('body-parser');
 
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.set('X-Auth-Required', 'true');
+  req.session.returnUrl = req.originalUrl;
+  res.status(401).send({"loginURL":"/login"});
+}
+
 module.exports = function(app){
 
 	app.use(bodyParser.urlencoded({ extended: false }));
 	app.use(bodyParser.json());
 
 	var gameController = require('./controllers/gameController')(app);
+  
+  	app.all('/api*', ensureAuthenticated);
 
-	app.post('/game', gameController.createGame);
-	app.get('/game/:id', gameController.getGame);
+	app.post('/api/game', gameController.createGame);
+	app.get('/api/game/:id', gameController.getGame);
 };
